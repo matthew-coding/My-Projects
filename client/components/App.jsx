90fa4557bc0a5cdeefc25f58346f4a616a082fa1
Matthew Kalopulu <matthew_kalopulu@hotmail.com>
2020-05-18 15:55:13 +1200

    add header component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './Header'
+
 
 const App = () => {
-  return (
-    <h1>React development has begun!</h1>
+  return ( 
+    <Header />
   )
 }
 
---
+++ client/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Matthew Kalopulu's coding nightmare</h1>
+        </div>
+    )
+}
+
+export default Header
