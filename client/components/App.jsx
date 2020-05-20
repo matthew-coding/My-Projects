0a35e4605ff74cf7950182444dc0e4369cbd5dd8
Matthew Kalopulu <matthew_kalopulu@hotmail.com>
2020-05-20 15:57:04 +1200

    add home component and contents

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './Header'
-
+import Home from './Home'
 
 const App = () => {
   return (
     <div className='app'>
       <Header />
+      <Home />
     </div>
   )
 }
---
+++ client/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is where I practice coding</h2>
+            <h3>My goals for this website:</h3>
+            <ul>
+                <li>
+                    <h4>
+                        Use React to build a functioning webpage
+                    </h4>
+                </li>
+                <li>
+                    <h4>
+                        Use Redux or Mobx, or anything that will manage the state of components
+                    </h4>
+                </li>
+                <li>
+                    <h4>
+                        Incorporate CSS that doesnt make me look stupid
+                    </h4>
+                </li>
+                <li>
+                    <h4>
+                        Have a fun game to play. Use phaser? Not 100% on that yet
+                    </h4>
+                </li>
+            </ul>
+        </div>
+    )
+}
